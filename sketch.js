447c9e1f71c4e767a649072c5806ee2fd4a29d0c
Jc-Arroyo <jcarroyos@users.noreply.github.com>
2024-02-17 14:20:10 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(320, 240);
+  
+}
+
+function draw() {
+  background(180,20,35);
+  circle(160,120,180);
+  circle(130,100,10);
+  circle(190,100,10);
+  rect(130,160,60,10);
+
+}
